8d5810eb6dd1e721fefcb03ec973a74a3d60f814
Noel Lobo <noelflobo@gmail.com>
2020-08-15 20:10:57 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,8 @@
+# Copyright (C) 2020 Noel Francis Lobo
+# codellectual@gmail.com
+# Created by Noel Lobo
+# (codellect - instagram | coffeestains - github | artstation.com/noelflobo)
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3 of the License, or
@@ -10,7 +15,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
-# Copyright (C) 2020 Noel Francis Lobo
+# 
+#
 # Have fun :)
 
 bl_info = {
